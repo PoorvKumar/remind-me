9c8dd827a2f59cf745650c23bbe243715f272f25
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2024-09-02 20:16:05 +0530

    feat: firebase env

--- api/src/config/firebase.ts
+++ api/src/config/firebase.ts
@@ -1,8 +1,11 @@
 import * as admin from "firebase-admin";
 import * as serviceAccount from "./firebase-adminsdk.json";
+import dotenv from "dotenv";
+dotenv.config();
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+    // credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+    credential: admin.credential.applicationDefault()
 });
 
 export default admin;
